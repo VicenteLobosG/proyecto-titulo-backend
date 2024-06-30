e826427d14ab73b6124d7ea285ed82e4cf87543f
VicenteLobosG <vicentelobosg@gmail.com>
2024-06-29 22:45:46 -0400

    add: crud categoria

--- control_productos/models/category.py
+++ control_productos/models/category.py
@@ -3,3 +3,7 @@ from django.db.models import Model, CharField
 class Category(Model):
     name = CharField(max_length=100)
     description = CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+    
